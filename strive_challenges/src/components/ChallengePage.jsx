fc59bb3095f791aff8f0790c0d4ad5f0339c3074
AnthonyBattista02 <tonybatt2002@gmail.com>
2023-11-21 13:55:40 -0600

    challengePage

--- strive_challenges/src/components/ChallengePage.jsx
+++ strive_challenges/src/components/ChallengePage.jsx
@@ -1,11 +1,15 @@
-
+import {Link} from 'react-router-dom'
 
 export default function ChallengePage () {
 
 
     return(
         <div>
-            Challenge Page
+            <div className='promptContainer'>
+                <div className='promptBox'> Prompt Box </div>
+                <textarea Style='height: 294px; vertical-align: super' className="code" placeholder="Type or Copy/Paste Code Answer Here"/>
+                <Link to='/' Style='margin-top: 15px; margin-right: 0px' className='submitButton'> Submit </Link>
+            </div>
         </div>
     )
 }
